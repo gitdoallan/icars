a3b793a1117fd57263b8945eac827192e6feeab7
Allan <mailtoallan@icloud.com>
2022-10-07 19:46:52 -0300

    feat: bike router config

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const userRouter = require('./user.routes');
+const bikesRouter = require('./bike.routes');
 
 const router = express.Router();
 
 router.use('/images', express.static('./src/images'));
 
 router.use('/users', userRouter);
+
+router.use('/bikes', bikesRouter);
 
 module.exports = router;
