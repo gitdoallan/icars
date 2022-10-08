40a69a0d6cbdf1b40141bffe75e326d00c4a04f1
Allan <mailtoallan@icloud.com>
2022-10-08 17:18:40 -0300

    refactor: skeleton logic using optional chaining

--- frontend/src/pages/BikeDetails/BikeDetails.jsx
+++ frontend/src/pages/BikeDetails/BikeDetails.jsx
@@ -1,9 +1,37 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { Header } from 'components/Header';
+import { Footer } from 'components/Footer';
+import { StatusMessages } from 'components/StatusMessages';
 import { getBikeById } from 'api';
 
 export function BikeDetails() {
+  const [bike, setBike] = useState(null);
+  const [status, setStatus] = useState({ status: false });
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    getBikeById(id)
+      .then((response) => setBike(response))
+      .catch(({ message }) => setStatus({ status: true, message, type: 'error' }));
+  }, [id]);
+
+  console.log(bike);
+
   return (
-    <div>BikeDetails</div>
+    <>
+      <Header />
+      {bike
+        ? (
+          <>
+            <h1>BikeDetails</h1>
+            <StatusMessages {...status} />
+          </>
+        )
+        : (<span>Loading...</span>)}
+
+      <Footer />
+    </>
   );
 }
--- frontend/src/pages/Store/Store.jsx
+++ frontend/src/pages/Store/Store.jsx
@@ -8,7 +8,7 @@ import { getAllBikes } from 'api';
 import * as S from './styles';
 
 export function Store() {
-  const [bikesList, setBikesList] = useState(null);
+  const [bikesList, setBikesList] = useState();
   const [status, setStatus] = useState({ status: false });
   const skeletonArray = Array.from(Array(6).keys());
   useEffect(() => {
@@ -23,9 +23,10 @@ export function Store() {
       <S.Title>Bike Listings</S.Title>
       <StatusMessages {...status} />
       <S.StoreContainer>
-        { bikesList
-          ? bikesList.map((bike) => (<BikesCard key={bike.id} {...bike} />))
-          : skeletonArray.map((e) => (<BikesCardSkeleton key={e} />)) }
+        {
+          bikesList?.map((bike) => (<BikesCard key={bike.id} {...bike} />))
+          || skeletonArray.map((e) => (<BikesCardSkeleton key={e} />))
+        }
       </S.StoreContainer>
       <Footer />
     </>
