4123a2d83db734c34062bda1edd0aac76c865b2e
Allan <mailtoallan@icloud.com>
2022-10-09 04:04:48 -0300

    feat: listing store location and model name

--- backend/src/services/store.service.js
+++ backend/src/services/store.service.js
@@ -1,7 +1,19 @@
-const { bikes: bikesModel } = require('../database/models');
+const { bikes, storeLocations, bikeModels } = require('../database/models');
 
 const getAllBikes = async () => {
-  const result = await bikesModel.findAll();
+  const result = await bikes.findAll({
+    attributes: ['id', 'image', 'price', 'rating'],
+    include: [
+      {
+        model: storeLocations,
+        attributes: ['name'],
+      },
+      {
+        model: bikeModels,
+        attributes: ['name'],
+      },
+    ],
+  });
   return result;
 };
 
