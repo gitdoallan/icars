051450ce9a5e2f65fa2e310fb93dac1551b95493
Allan <mailtoallan@icloud.com>
2022-10-09 15:28:40 -0300

    fix: getReservationById is now being called correctly

--- backend/src/controllers/reservations.controller.js
+++ backend/src/controllers/reservations.controller.js
@@ -8,7 +8,7 @@ const listAllUserReservations = async (req, res) => {
 const getReservationById = async (req, res) => {
   const { orderId } = req.params;
   const { id, role } = req.user;
-  const result = await reservationsService.getAllReservations({
+  const result = await reservationsService.getReservationById({
     orderId, userId: id, role,
   });
   res.status(200).json(result);
