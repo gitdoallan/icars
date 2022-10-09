5b06e80564cf1b6dd1226065da50847777916590
Allan <mailtoallan@icloud.com>
2022-10-09 19:12:54 -0300

    chore: removing dummy data

--- frontend/src/api/store/rentBike.store.api.js
+++ frontend/src/api/store/rentBike.store.api.js
@@ -2,11 +2,10 @@ import axios from 'axios';
 import { API_URL } from '../apiUrl';
 
 export const rentBike = async ({ id, startDate, endDate }) => {
-  const orderTotal = 1000;
   const { data } = await axios.post(
     `${API_URL}/store/bike/rent`,
     {
-      id, orderTotal, startDate, endDate,
+      id, startDate, endDate,
     },
     { withCredentials: true },
   );
