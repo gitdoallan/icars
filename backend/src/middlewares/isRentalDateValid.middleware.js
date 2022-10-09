01405d64ced813401cb26a87c21024dfa1431fea
Allan <mailtoallan@icloud.com>
2022-10-09 17:49:40 -0300

    fix: now the logic only grabs date

--- backend/src/middlewares/isRentalDateValid.middleware.js
+++ backend/src/middlewares/isRentalDateValid.middleware.js
@@ -2,12 +2,13 @@ const { ErrorHandler } = require('../utils/errorHandler');
 
 const isRentalDateValid = async (req, _res, next) => {
   const { startDate, endDate } = req.body;
-  const startDateObj = new Date(startDate);
-  const endDateObj = new Date(endDate);
-  if (startDateObj > endDateObj) {
+  const today = new Date().getDate();
+  const startDay = new Date(startDate).getDate();
+  const endDay = new Date(endDate).getDate();
+  if (startDay > endDay) {
     throw new ErrorHandler(400, 'Start date cannot be greater than end date');
   }
-  if (startDateObj < new Date()) {
+  if (startDay < today) {
     throw new ErrorHandler(400, 'Start date cannot be in the past');
   }
   next();
--- backend/src/routes/store.routes.js
+++ backend/src/routes/store.routes.js
@@ -9,7 +9,7 @@ router.use(verifyToken);
 
 router.get('/bikes', storeController.getAllBikes);
 router.post('/bike/rent', isRentalDateValid, storeController.rentBike);
-router.post('/bike/available', storeController.isBikeAvailable);
+router.post('/bike/available', isRentalDateValid, storeController.isBikeAvailable);
 router.get('/bike/:id', storeController.getBikeById);
 
 module.exports = router;
