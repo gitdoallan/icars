3404afbb7ae8c02abb22939f610e3eead5a944ce
Allan <mailtoallan@icloud.com>
2022-10-12 14:33:20 -0300

    feat: generic created page implemented (no design yet)

--- frontend/src/pages/Admin/Created/Created.jsx
+++ frontend/src/pages/Admin/Created/Created.jsx
@@ -1,11 +1,38 @@
 import { Header } from 'components/Header';
+import { useParams, useNavigate, Link } from 'react-router-dom';
 import { Footer } from 'components/Footer';
 
 export function Created() {
+  const { type, id } = useParams();
+  const navigate = useNavigate();
+
+  const cases = {
+    bike: {
+      title: 'Bike Created',
+      message: 'Your bike has been created successfully',
+      link: `/store/bike/${id}`,
+      linkText: 'View Bike',
+      createAnother: '/admin/bike/new',
+    },
+    user: {
+      title: 'User Created',
+      message: 'The user has been created successfully',
+      link: `/admin/user/${id}`,
+      linkText: 'View User',
+      createAnother: '/admin/user/new',
+    },
+  };
+
+  if (!cases[type]) navigate('/store');
+
   return (
     <>
       <Header />
-      <h1>Created</h1>
+      <h1>{cases[type].title}</h1>
+      <p>{cases[type].message}</p>
+      <Link to={cases[type].link}>{cases[type].linkText}</Link>
+      {' | '}
+      <Link to={cases[type].createAnother}>Create Another</Link>
       <Footer />
     </>
   );
--- frontend/src/routes/Router.jsx
+++ frontend/src/routes/Router.jsx
@@ -1,10 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Admin, AdminUserManager, CreateNewBike } from 'pages/Admin';
 import { Store, BikeDetails } from 'pages/Store';
 import { Registration } from 'pages/Registration';
 import { Reservations, ReservationDetails } from 'pages/Reservations';
 import { Logout } from 'pages/Logout';
 import { NotFound } from 'pages/NotFound';
+
+import {
+  Admin, AdminUserManager, CreateNewBike, Created,
+} from 'pages/Admin';
 
 export function Router() {
   return (
@@ -18,6 +21,7 @@ export function Router() {
         <Route path="/admin" element={<Admin />} />
         <Route path="/admin/user/:id" element={<AdminUserManager />} />
         <Route path="/admin/bike/new" element={<CreateNewBike />} />
+        <Route path="/admin/:type/new/:id" element={<Created />} />
         <Route path="/logout" element={<Logout />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
