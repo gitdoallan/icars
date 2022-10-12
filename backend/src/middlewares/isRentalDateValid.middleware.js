164fafe27a05eacdbcb3a25bd0894c3781329149
Allan <mailtoallan@icloud.com>
2022-10-11 22:12:58 -0300

    fix: checking if selected date is yesterday

--- backend/src/middlewares/isRentalDateValid.middleware.js
+++ backend/src/middlewares/isRentalDateValid.middleware.js
@@ -1,14 +1,15 @@
 const { ErrorHandler } = require('../utils/errorHandler');
+const { ONE_DAY } = require('../utils/magicNumbers');
 
 const isRentalDateValid = async (req, _res, next) => {
   const { startDate, endDate } = req.body;
-  const today = new Date().getTime();
-  const startDay = new Date(startDate).getTime();
-  const endDay = new Date(endDate).getTime();
+  const yesterday = new Date().setHours(0, 0, 0, 0) - ONE_DAY;
+  const startDay = new Date(startDate).setHours(0, 0, 0, 0);
+  const endDay = new Date(endDate).setHours(0, 0, 0, 0);
   if (startDay > endDay) {
     throw new ErrorHandler(400, 'Start date cannot be greater than end date');
   }
-  if (startDay < today) {
+  if (startDay < yesterday) {
     throw new ErrorHandler(400, 'Start date cannot be in the past');
   }
   next();
