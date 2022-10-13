3ae70653792698af018203ddcbc455d1ae555094
Allan <mailtoallan@icloud.com>
2022-10-12 22:12:07 -0300

    feat: backend update user function implemented

--- backend/src/routes/admin.routes.js
+++ backend/src/routes/admin.routes.js
@@ -9,6 +9,7 @@ router.use(verifyAdminToken);
 router.get('/', adminController.listAllReservations);
 router.get('/users', adminController.listAllUsers);
 router.post('/user/new', adminController.createNewAccount);
+router.put('/user/:id', adminController.updateUserById);
 router.get('/user/:id', adminController.getAllReservationsByUserId);
 router.delete('/user/:id', adminController.deleteUserById);
 router.get('/bikes', adminController.listAllBikes);
