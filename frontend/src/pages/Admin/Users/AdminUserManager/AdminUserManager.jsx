e397d5e26d78b53d8c91e9bef0969f32958c2962
Allan <mailtoallan@icloud.com>
2022-10-12 21:18:39 -0300

    feat: admin user details updated

--- frontend/src/pages/Admin/Users/AdminUserManager/AdminUserManager.jsx
+++ frontend/src/pages/Admin/Users/AdminUserManager/AdminUserManager.jsx
@@ -2,33 +2,66 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Header } from 'components/Header';
 import { Footer } from 'components/Footer';
+import { StatusMessages } from 'components/StatusMessages';
 import { getAllReservationsByUserId, deleteUsersById } from 'api';
 
 export function AdminUserManager() {
   const [reservations, setReservations] = useState([]);
+  const [status, setStatus] = useState({ status: false });
   const { id } = useParams();
 
   const handleDeleteUser = async () => {
     try {
       const result = await deleteUsersById(id);
-      console.log(result);
-    } catch {
-      console.log('error');
+      setStatus({ status: true, message: result.message, type: 'success' });
+    } catch (err) {
+      setStatus({ status: true, message: err.message, type: 'error' });
     }
   };
 
   useEffect(() => {
     getAllReservationsByUserId(id)
       .then(setReservations)
-      .catch(console.error);
+      .catch((err) => setStatus({ status: true, message: err.message, type: 'error' }));
   }, []);
 
-  console.log(reservations);
   return (
     <div>
       <Header />
       <h1>Manage User</h1>
+      <StatusMessages {...status} />
       <button type="button" onClick={handleDeleteUser}>Delete user</button>
+      <h2>Reservations</h2>
+      {reservations?.map((reservation) => (
+        <div key={reservation.id}>
+          <p>
+            Order ID:
+            {' '}
+            {reservation.id}
+          </p>
+          <p>
+            Start Date:
+            {' '}
+            {reservation.startDate.split('T')[0]}
+          </p>
+          <p>
+            End Date:
+            {' '}
+            {reservation.endDate.split('T')[0]}
+          </p>
+          <p>
+            Status:
+            {' '}
+            {reservation.orderStatus}
+          </p>
+          <p>
+            Total:
+            {' '}
+            {reservation.orderTotal}
+          </p>
+          <hr />
+        </div>
+      ))}
       <Footer />
     </div>
   );
