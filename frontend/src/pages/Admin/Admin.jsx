bac5cc0e72a56eb534abb176fce16c1be2d51287
Allan <mailtoallan@icloud.com>
2022-10-12 21:48:54 -0300

    feat: admin/reservations page to show error instead of console.log

--- frontend/src/pages/Admin/Admin.jsx
+++ frontend/src/pages/Admin/Admin.jsx
@@ -2,20 +2,23 @@ import { useState, useEffect } from 'react';
 import { Header } from 'components/Header';
 import { Footer } from 'components/Footer';
 import { AdminReservationsCard } from 'components/Cards';
+import { StatusMessages } from 'components/StatusMessages';
 import { listAllReservations } from 'api';
 
 export function Admin() {
   const [reservations, setReservations] = useState([]);
+  const [status, setStatus] = useState({ status: false });
   useEffect(() => {
     listAllReservations()
       .then(setReservations)
-      .catch(({ response }) => console.log(response.data.message));
+      .catch(({ response }) => setStatus({ status: true, message: response.data.message, type: 'error' }));
   }, []);
 
   return (
     <div>
       <Header />
       <h1>Reservations</h1>
+      <StatusMessages {...status} />
       {reservations.map((reservation) => (
         <AdminReservationsCard key={reservation.id} {...reservation} />
       ))}
